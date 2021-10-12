58759d9c9abdc5e1aee51ef733934e58e601a0e8
LEE SUJIN <sujin0970@naver.com>
2021-10-13 06:27:04 +0900

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://kbsc-backend-spring.run.goorm.io';
-// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://34.122.110.59';
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://kbsc-backend-spring.run.goorm.io';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://34.122.110.59:8080';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const NAME_MIN_LENGTH = 1;
